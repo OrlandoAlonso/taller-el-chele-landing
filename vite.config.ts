f01e21a7e9b73a56f60767a29b8680928ef86538
OrlandoAlonso <navarreteorlando57@gmail.com>
2026-02-07 19:52:01 -0600

    corregir base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    base: "/taller-el-chele-landing/",
+    base: mode === "production" ? "/taller-el-chele-landing/" : "/",
     host: "::",
     port: 8080,
     hmr: {
